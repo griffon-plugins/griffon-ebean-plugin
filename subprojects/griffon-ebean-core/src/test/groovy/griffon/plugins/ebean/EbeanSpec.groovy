63e4b26aaea1fab2b89819f0780341de57471709
aalmiray <aalmiray@gmail.com>
2017-06-01 11:22:07 +0200

    Update to latest Griffon

--- subprojects/griffon-ebean-core/src/test/groovy/griffon/plugins/ebean/EbeanSpec.groovy
+++ subprojects/griffon-ebean-core/src/test/groovy/griffon/plugins/ebean/EbeanSpec.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 the original author or authors.
+ * Copyright 2014-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@ package griffon.plugins.ebean
 
 import com.avaje.ebean.EbeanServer
 import griffon.core.GriffonApplication
+import griffon.core.RunnableWithArgs
 import griffon.core.test.GriffonUnitRule
 import griffon.inject.BindTo
 import griffon.plugins.ebean.exceptions.RuntimeEbeanServerException
@@ -53,9 +54,9 @@ class EbeanSpec extends Specification {
         ]
         List events = []
         eventNames.each { name ->
-            application.eventRouter.addEventListener(name) { Object... args ->
+            application.eventRouter.addEventListener(name, ({ Object... args ->
                 events << [name: name, args: args]
-            }
+            } as RunnableWithArgs))
         }
 
         when:
